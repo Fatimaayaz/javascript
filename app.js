5712109d459e9d478e618d3fce0bf875d87b1d40
Fatima <64702735+Fatimaayaz@users.noreply.github.com>
2020-06-07 16:52:23 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+// ____________1_____________  
+
+ alert("Hey!!  welcome to the alert box of JavaScript :)" )
+
+
+ // ____________2_____________  
+
+     var name = +prompt ( "Enter password" )
+     alert("Error!! Please enter a valid password")
+
+
+
+
+ // ____________3_____________  
+
+  alert("Welcome To JS Land.\nHappy Coding!" );
+
+  // ____________4_____________  
+
+alert ("Happy Coding \n prevent this page from creating additional dialogs")
+
+
+ // ____________5_____________  
+alert ("Hello... I can run JS through my web browser's console");
+ console.log( alert );
+
